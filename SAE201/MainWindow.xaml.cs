58e0e017a0ec4caead279adb0fbffdf96074c383
mateomrt <mateo.martin@etu.univ-smb.fr>
2023-06-16 08:24:15 +0200

    suppr

--- SAE201/MainWindow.xaml.cs
+++ SAE201/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace SAE201
     public partial class MainWindow : Window
     {
 
-        private Window pageajout;
+        private Window pageAjout;
         private Window pageModif;
         public MainWindow()
         {
@@ -79,6 +79,7 @@ namespace SAE201
                 if (result == MessageBoxResult.Yes)
                 {
                     ((Personnel)listViewPersonnel.SelectedItem).Delete();
+                    
                     MessageBox.Show("Bien supprimé", "Suppression", MessageBoxButton.OK);
                 }
                 else
@@ -90,26 +91,26 @@ namespace SAE201
 
         private void Button_Click(object sender, RoutedEventArgs e) // AJOUT PERSONNEL
         {
-            pageajout = new Ajout();
-            pageajout.Show();
+            pageAjout = new Ajout();
+            pageAjout.Show();
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e) // AJOUT ATTRIBUTION
         {
-            pageajout = new Ajout();
-            pageajout.Show();
+            pageAjout = new Ajout();
+            pageAjout.Show();
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e) // AJOUT MATERIEL
         {
-            pageajout = new Ajout();
-            pageajout.Show();
+            pageAjout = new Ajout();
+            pageAjout.Show();
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e) // AJOUT CATEGORIE
         {
-            pageajout = new Ajout();
-            pageajout.Show();
+            pageAjout = new Ajout();
+            pageAjout.Show();
         }
     }
 }
